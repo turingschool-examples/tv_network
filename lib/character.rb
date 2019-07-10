3eda6fae4b7723f64fa655a7126aad3c59bc3b52
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-10 09:25:38 -0600

    Create character class with attributes, tested

---
+++ lib/character.rb
@@ -0,0 +1,12 @@
+require 'pry'
+
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(person)
+    @name = person [:name]
+    @actor = person [:actor]
+    @salary = person [:salary]
+  end
+  
+end
---
+++ test/character_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require 'pry'
+
+class CharacterTest < MiniTest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  def test_it_exist
+
+    assert_instance_of Character, @kitt
+  end
+
+  def test_character_attributes
+
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1_000_000, @kitt.salary
+  end
+end
