7132ba2d4027052f171c965f7298a37973d51e24
Tylor Schafer <tylorschafer@gmail.com>
2019-07-10 09:20:19 -0600

    Add show class file and full functionality

--- test/show_test.rb
+++ test/show_test.rb
@@ -8,7 +8,7 @@ class ShowTest < Minitest::Test
   def setup
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
   end
 
   def test_instance_of_show
