8f97ac6b2718bbb445bd12ef4ff735893073510e
Evette Telyas <etelyas@gmail.com>
2019-07-10 10:07:45 -0600

    fix indentation on lib files

--- lib/network.rb
+++ lib/network.rb
@@ -1,38 +1,35 @@
 class Network
 attr_reader :name, :shows
 
-def initialize(name, shows = [])
-  @name = name
-  @shows = shows
-end
+  def initialize(name, shows = [])
+    @name = name
+    @shows = shows
+  end
 
-def add_show(show)
-  @shows.push(show)
-end
+  def add_show(show)
+    @shows.push(show)
+  end
 
-def highest_paid_actor
-  characters_in_shows = []
-  @shows.map do |show|
-    show.characters.each do |character|
-      characters_in_shows << character
+  def highest_paid_actor
+    characters_in_shows = []
+    @shows.map do |show|
+      show.characters.each do |character|
+        characters_in_shows << character
+      end
     end
-  end
-
-  highest_paid_actor_object =                          characters_in_shows.max_by do |character|    character.salary
-  end
-
-  highest_paid_actor_object.actor
-end
-
-def payroll
-  payroll = {}
-  @shows.map do |show|
-    show.characters.each do |character|
-      payroll[character.actor] = character.salary
+    highest_paid_actor_object =                          characters_in_shows.max_by do |character|    character.salary
     end
+    highest_paid_actor_object.actor
   end
-  payroll
-end
 
-
+  def payroll
+    payroll = {}
+    @shows.map do |show|
+      show.characters.each do |character|
+        payroll[character.actor] = character.salary
+      end
+    end
+    payroll
+  end
+  
 end
--- lib/show.rb
+++ lib/show.rb
@@ -1,15 +1,14 @@
 class Show
   attr_reader :name, :creator, :characters
 
-  def initialize(name, creator, characters)
-    @name = name
-    @creator = creator
-    @characters = characters
-  end
+    def initialize(name, creator, characters)
+      @name = name
+      @creator = creator
+      @characters = characters
+    end
 
-  def total_salary
-    characters.inject(0) {|salary, character| salary + character.salary}
-  end
-
+    def total_salary
+      characters.inject(0) {|salary, character| salary + character.salary}
+    end
 
 end
