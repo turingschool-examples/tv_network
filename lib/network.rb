86d9fe5d935bb80451e16a711c3e6035444648ab
Alec Wells <alec.wells47@gmail.com>
2019-07-10 10:13:18 -0600

    Finish iteration 3

---
+++ lib/network.rb
@@ -0,0 +1,29 @@
+class Network
+  attr_reader :name, :shows
+
+  def initialize(name, shows = [])
+    @name = name
+    @shows = shows
+  end
+
+  def add_show(show)
+
+    @shows << show
+  end
+
+  def highest_paid_actor
+    @highest_salary = 0
+    @highest_salary_name = ""
+    #maybe use sort_by or max
+    @shows.each do |show|
+      show.characters.each do |character|
+        if character.salary > @highest_salary
+          @highest_salary = character.salary
+          @highest_salary_name = character.actor
+        end
+      end
+    end
+    @highest_salary_name
+  end
+
+end
---
+++ test/network_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+require 'pry'
+
+class NetworTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @nbc = Network.new("NBC")
+  end
+
+  def test_it_has_name
+
+    assert_equal "NBC", @nbc.name
+  end
+
+  def test_shows_empty
+
+    assert_equal [], @nbc.shows
+  end
+
+  def test_can_add_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_has_highest_paid_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    
+    assert_equal "Amy Poehler", @nbc.highest_paid_actor
+  end
+
+end
