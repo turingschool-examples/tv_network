8ddffde370ce18e980b47657bd643cbf9a11d55a
Daniel <danmoran.pro@gmail.com>
2019-07-10 10:35:31 -0600

    Add show test file and show file.

---
+++ lib/show.rb
@@ -0,0 +1,10 @@
+class Show
+
+attr_reader :name, :creator
+
+  def initialize(character, creator, actors)
+    @name = character
+    @creator = creator
+  end
+
+end
---
+++ test/show_test.rb
@@ -0,0 +1,26 @@
+require './lib/character'
+require './lib/show'
+require 'minitest/autorun'
+require 'minitest/pride'
+require "pry"
+
+class ShowTest < Minitest::Test
+kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+michael_knight =Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight =Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_rider_has_name
+    assert_equal "Knight Rider", @knight_rider.name
+  end
+
+  def test_creator_has_name
+    assert_equal "Glen Larson", @knight_rider.creator
+  end
+end
